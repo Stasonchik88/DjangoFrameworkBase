497e8f38ce33cf43f9d151d30d58069f53726c9b
Stasonchik <poselenov_stas@mail.ru>
2022-02-08 23:46:01 +0300

    HomeWork_7
    Домашнее задание к уроку №7

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -13,11 +13,13 @@ MENU_LINKS = [
 # Create your views here.
 def main(request):
     products = Product.objects.all()[:4]
+    basket = get_basket(request.user)
 
     return render(request, 'mainapp/index.html', context={
         'title': 'Главная',
         'menu_links': MENU_LINKS,
         'products': products,
+        'basket': basket,
     })
     
 
@@ -28,15 +30,14 @@ def get_basket(user):
         return []
 
         
-def get_hot_product():
-    products = Product.objects.all()
+def get_hot_product(category):
+    products = Product.objects.filter(category=category)
 
     return random.sample(list(products), 1)[0]
     
     
 def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category).\
-                                    exclude(pk=hot_product.pk)[:4]
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:4]
 
     return same_products
 
@@ -44,16 +45,16 @@ def get_same_products(hot_product):
 def products(request, pk=None):
     categories = ProductCategory.objects.all()
     category = {'name': 'все'}
-    products = Product.objects.all()[:4]
-
+    hot_product = Product.objects.all()[0]
+    products = Product.objects.all()[1:5]
+    basket = get_basket(request.user)
+    
     if pk is not None and pk != 0:
         category = get_object_or_404(ProductCategory, pk=pk)
         products = Product.objects.filter(category__pk=pk).order_by('price')
-
-    basket = get_basket(request.user)
-    hot_product = get_hot_product()
-    products = get_same_products(hot_product)
-
+        hot_product = get_hot_product(category)
+        products = get_same_products(hot_product)
+    
     return render(request, 'mainapp/products.html', context={
         'title': 'Продукты',
         'menu_links': MENU_LINKS,
